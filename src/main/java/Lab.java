ff347ec67156baaddf30b27ea8054d44fc487d8f
Charles V <charlesvaldezz94@gmail.com>
2023-02-27 22:49:53 +0000

    test passed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -26,7 +27,8 @@ public class Lab {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        Map <Integer, String> hashes = new HashMap<>();
+        return hashes;
     }
 
     /**
@@ -36,7 +38,8 @@ public class Lab {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        int mapSize = map.size();
+        return mapSize;
     }
 
     /**
@@ -47,6 +50,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key, value);
     }
 
     /**
@@ -56,7 +60,8 @@ public class Lab {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        String mapValue = map.get(key);
+        return mapValue;
     }
 
     /**
@@ -66,6 +71,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
@@ -76,6 +82,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
-
+        map.replace(key, value);
     }
 }
